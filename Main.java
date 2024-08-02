a7821ba35f5d773195d282004e6327708dddbe91
Utkarsh Mishra <utkarsh_mishra@epam.com>
2024-08-02 10:59:16 +0530

    list added for tag

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     HashSet<Integer> ser = new HashSet<>();
+    HashMap<> map = new HashMap<>();
     List<Integer> ls = new ArrayList<>();
   }
 }
